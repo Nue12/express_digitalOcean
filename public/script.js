3656cc63631efe40d0f72a333a2e2b878e2deffb
Nue <phonehtetaung1993@gmail.com>
2023-02-15 13:16:56 +0630

    change to script.js file

--- public/script.js
+++ public/script.js
@@ -1,28 +1,21 @@
 const API = localStorage.getItem("apiUrl");
-const fetchData = async () => {
-  if (API) {
-    const response = await fetch(`${API}`);
-    const data = await response.json();
-    console.log(data);
-  } else {
-    window.location.href = "/api";
-  }
-};
-
-fetchData();
 
 const imgParentTag = document.querySelector(".imgParent");
 
 const uploadFiles = async () => {
   const fileForm = document.getElementById("fileForm");
   const formData = new FormData(fileForm);
-  const response = await fetch(`${API}/upload`, {
-    method: "POST",
-    body: formData,
-  });
-  const data = await response.json();
-  console.log(data);
-  const imgTag = document.createElement("img");
-  imgTag.src = data.fileData;
-  imgParentTag.append(imgTag);
+  if (API) {
+    const response = await fetch(`${API}/upload`, {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    console.log(data);
+    const imgTag = document.createElement("img");
+    imgTag.src = data.fileData;
+    imgParentTag.append(imgTag);
+  } else {
+    window.location.href = "/api";
+  }
 };
